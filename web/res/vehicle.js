4f8b77f86ab548d914c7ada99dcf1704b34a7ab7
Muhammed Shakkir <shakkirptb@gmail.com>
2019-10-02 06:29:40 +0530

    removed sliding marker as it is CPU intensive implemented custom logic for marker animation

--- web/res/vehicle.js
+++ web/res/vehicle.js
@@ -1,89 +1,122 @@
 'use strict';
 
-const Vehicle = function(id,location,map){
-    this.id=id;
-    this.location=location;
-    this.marker=null;
+const Vehicle = function (id, location, map) {
+    this.id = id;
+    this.location = location;
+    this.marker = null;
     //rest of the contructure code
-    this.create(id,location,map);
-   
+    this.create(id, location, map);
+
 }
 /**create marker*/
-Vehicle.prototype.create=function(id,location,map){
-    this.angle=getBearingAngle(location,CENTER);
-    this.marker= new SlidingMarker({
+Vehicle.prototype.create = function (id, location, map) {
+    this.angle = getBearingAngle(location, CENTER);
+    this.marker = new google.maps.Marker({
         position: location,
+        optimized: false,
         map: map,
         icon: getCarIcon(this.angle),
         title: "Car: " + id,
         duration: UPDATE_RATE
-    });  
+    });
 }
 /**remove marker from map */
-Vehicle.prototype.destroy=function(){
+Vehicle.prototype.destroy = function () {
     this.marker.setMap(null);
 }
+/**gradual space transition with the help of sling marker*/
+var precision = 30;
+var anglePrec = 10;
 /**move marker */
-Vehicle.prototype.moveTo=function(newLocation){
-    if(this.location && newLocation){
-        if(! isSame(this.location,newLocation)){
-            this.marker.setPosition(newLocation);
-            this.animateRotation(newLocation);
-            this.location=newLocation;
+Vehicle.prototype.moveTo = function (newLocation) {
+    if (this.location && newLocation) {
+        if (!isSame(this.location, newLocation)) {
+             //animate displacement
+            this.newLocation = newLocation;
+            this.dLat = (newLocation.lat - this.location.lat)/precision;
+            this.dLng = (newLocation.lng - this.location.lng)/precision;
+            //animate bearing/angle transition;
+            this.bearingTo = getBearingAngle(this.location, newLocation);
+            this.angleToTurn = this.bearingTo - this.angle;
+            this.angleToTurn = Math.abs(this.angleToTurn) > 180 ? (this.angle >= 0 ? (360 - this.angleToTurn):-(360 - this.angleToTurn)) : this.angleToTurn;
+            this.deltaAngle = this.angleToTurn/ anglePrec;//angle should change faster for better visual experience
+            //
+            animateVehicle(this);
         }
     }
 }
-/**gradual space transition with the help of sling marker*/
-Vehicle.prototype.animateRotation = function(newLocation){
-    //animate  space and angle transition;
-    this.bearingTo =getBearingAngle(this.location,newLocation);
-    this.angleToTurn = this.bearingTo-this.angle;
-    if(Math.abs(this.angleToTurn) < 5){
-        this.marker.setIcon(getCarIcon(this.bearingTo));
-        return;
-    }
-    this.angleToTurn = Math.abs(this.angleToTurn) > 180 ? -(360-this.angleToTurn) : this.angleToTurn;
-    this.deltaAngle =   this.angleToTurn/10;
-    startRotation(this);
-}
 /**performance improvement by avoiding many timers with one */
-var rTimer =null;
-var vehicles={};
-var interval=100;
-function startRotation(vehicle){
-    vehicles[vehicle.id] = vehicle;
-    if(rTimer == null){
-        rTimer = setInterval(()=>{
-            for(let id in vehicles){
-                var v =  vehicles[id];
-                v.angle = v.angle+v.deltaAngle;
-                if(Object.keys(vehicles).length > 20 || //performance measure
-                (v.deltaAngle > 0 && v.angle >= v.bearingTo)||
-                v.deltaAngle < 0 && v.angle <= v.bearingTo){
-                    v.angle=v.bearingTo;
-                    delete vehicles[id];
-                    if(Object.keys(vehicles).length==0){
-                        clearInterval(rTimer);   
-                        rTimer=null;                    
-                    }
+var rTimer = null;
+var animVehicles = {};
+var interval = 30;
+function animateVehicle(vehicle) {
+    animVehicles[vehicle.id] = vehicle;
+    if (rTimer == null) {
+        rTimer = setInterval(() => {
+            for (let id in animVehicles) {
+                var v = animVehicles[id];
+                //animation for latitude completed?
+                let latMoved = (v.dLat > 0 && v.location.lat >= v.newLocation.lat) || v.dLat < 0 && v.location.lat <= v.newLocation.lat;
+                if (!latMoved) {
+                    v.location.lat += v.dLat;//increment lat
                 }
-                v.marker.setIcon(getCarIcon(v.angle));
-            }  
-        },interval); 
+                //animation for longitude completed?
+                let lngMoved = (v.dLng > 0 && v.location.lng >= v.newLocation.lng) || v.dLng < 0 && v.location.lng <= v.newLocation.lng;
+                if (!lngMoved) {
+                    v.location.lng += v.dLng;//increment lng
+                }
+                if (!latMoved || !lngMoved) {
+                    v.marker.setPosition(v.location);
+                }
+                //animation for angle completed?
+                let turned = (v.deltaAngle > 0 && v.angle >= v.bearingTo) || v.deltaAngle < 0 && v.angle <= v.bearingTo;
+                if (!turned) {
+                    v.angle += v.deltaAngle;//increment angle
+                    v.marker.setIcon(getCarIcon(v.angle));
+                }
+                //------
+                if (latMoved && lngMoved && turned) {
+                    v.angle = v.bearingTo;
+                    v.location=v.newLocation;
+                    delete animVehicles[id];
+                    if (Object.keys(animVehicles).length == 0) {
+                        clearInterval(rTimer);
+                        rTimer = null;
+                    }
+                    v.marker.setIcon(getCarIcon(v.angle));
+                    v.marker.setPosition(v.location);
+                }
+                //------
+            }
+        }, interval);
     }
 }
 /**get car SVG icon for the given rotation */
-function getCarIcon(rotation){
+function getCarIcon(rotation) {
     CarIcon.rotation = rotation;
     return CarIcon;
 }
 /**Manage clustering */
-function isSame(loc1,loc2){
+function isSame(loc1, loc2) {
     return loc1.lat === loc2.lat && loc1.lng === loc2.lng
 }
 /**calculate bearing on movement */
-function getBearingAngle(lastPosn, dest){
+function getBearingAngle1(lastPosn, dest) {
+    /**bearing on 2D/plane */
     let x = dest.lng - lastPosn.lng;
     let y = dest.lat - lastPosn.lat;
-    return Math.round(Math.atan2(x,y)*57.296); // rad*180/pi;
+    return Math.round(rad2deg(Math.atan2(x, y))*100)/100; // rad*180/pi;
+}
+/**bearing on globe */
+function getBearingAngle(lastPosn, dest) {
+    let l2 = rad2deg(dest.lng);
+    let l1= rad2deg(lastPosn.lng);
+    let f2 = rad2deg(dest.lat);
+    let f1 = rad2deg(lastPosn.lat);
+    var y = Math.sin(l2-l1) * Math.cos(f2);
+    var x = Math.cos(f1)*Math.sin(f2) - Math.sin(f1)*Math.cos(f2)*Math.cos(l2-l1);
+    return  Math.round(rad2deg(Math.atan2(y, x))*100)/100;
+}
+function rad2deg(red) {
+    return red * 57.296;
 }
